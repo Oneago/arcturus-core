0d95a5c99c0a8f0fbf88a289f5517cf5db52fc5b
Juan David Rueda Quiroga <jrueda@oneago.com>
2021-01-27 16:46:08 -0500

    Add throw to ExampleModel.php

--- templates/ExampleModel.php
+++ templates/ExampleModel.php
@@ -4,26 +4,32 @@
 namespace App\Models;
 
 
+use BadMethodCallException;
+
 class ExampleModel extends Connection
 {
 
     public function get(int $id): ?object
     {
         // TODO: Implement get() method.
+        throw new BadMethodCallException("Get not enable");
     }
 
     public function create(object $model): bool
     {
         // TODO: Implement create() method.
+        throw new BadMethodCallException("Create not enable");
     }
 
     public function list(string $search = null): ?array
     {
         // TODO: Implement list() method.
+        throw new BadMethodCallException("List not enable");
     }
 
     public function update(object $model): bool
     {
         // TODO: Implement update() method.
+        throw new BadMethodCallException("Update not enable");
     }
 }
