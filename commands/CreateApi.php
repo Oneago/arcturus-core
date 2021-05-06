0537da8a3b71d1a85d7001899cfe575e09c35c8f
Juan David Rueda Quiroga <jrueda@oneago.com>
2021-05-05 21:03:31 -0500

    Add folder feature to api

--- commands/CreateApi.php
+++ commands/CreateApi.php
@@ -3,8 +3,10 @@
 
 namespace Oneago\Arcturus\Commands;
 
+use RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -17,18 +19,27 @@ class CreateApi extends Command
         $this
             ->setDescription("Create a new api for this app")
             ->addArgument("api name", InputArgument::REQUIRED, "Name for use in api file")
+            ->addOption("dir", "d", InputOption::VALUE_OPTIONAL, "Save view in a folder for pretty viewer")
             ->setHelp("This command create a new api passing a name");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $dir = ucfirst($input->getOption("dir"));
+
         $output->writeln("<info>Creating {$input->getArgument('api name')}</info>");
         $output->writeln("<info>Wait a moment please...</info>");
         $output->writeln("");
 
-        $apiName = ucfirst($input->getArgument('api name')) . "Api.php";
+        $apiName = $this->viewName = ($dir ?? "") . ucfirst($input->getArgument('api name')) . "Api.php";
         $output->writeln("<info>Creating $apiName</info>");
-        $this->createFile($apiName, __DIR__ . "/../templates/ExampleApi.php");
+
+        if ($dir !== null) {
+            $this->createFile($apiName, __DIR__ . "/../templates/ExampleApi.php", $dir);
+        } else {
+            $this->createFile($apiName, __DIR__ . "/../templates/ExampleApi.php", null);
+        }
+
 
         $output->writeln("<info>$apiName Created!</info>");
         $output->writeln("");
@@ -40,10 +51,17 @@ class CreateApi extends Command
     /**
      * @param string $name
      * @param string $templatePath
+     * @param string|null $newDirectory
      */
-    private function createFile(string $name, string $templatePath): void
+    private function createFile(string $name, string $templatePath, ?string $newDirectory): void
     {
         $savePath = "app/Http/Apis";
+        if ($newDirectory !== null) {
+            if (!mkdir("$savePath/$newDirectory") && !is_dir("$savePath/$newDirectory")) {
+                throw new RuntimeException(sprintf('Directory "%s" was not created', "$savePath/$newDirectory"));
+            }
+            $savePath = "$savePath/$newDirectory";
+        }
         $fp = fopen("$savePath/$name", 'wb+');
 
         $fileContent = file_get_contents($templatePath);
@@ -51,12 +69,12 @@ class CreateApi extends Command
         $contents = str_replace(
             [
                 "ExampleApi",
-                "App\Http\Controllers",
+                "App\Http\Apis",
                 " is a example class, you can delete or use as a model example for your app"
             ],
             [
                 str_replace(".php", "", $name),
-                "App\Http\Apis",
+                "App\Http\Apis" . ($newDirectory !== null ? "\\$newDirectory" : ''),
                 ""
             ], $fileContent
         );
--- templates/ExampleApi.php
+++ templates/ExampleApi.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace App\Http\Controllers;
+namespace App\Http\Apis;
 
 
 class ExampleApi
