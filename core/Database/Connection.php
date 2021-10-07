f5a10977f1770d5bce5cadaa988738946d09e686
Juan David Rueda Quiroga <jrueda@oneago.com>
2021-10-07 13:07:12 -0500

    Set type to Connection.php

--- core/Database/Connection.php
+++ core/Database/Connection.php
@@ -8,10 +8,7 @@ use PDO;
 
 abstract class Connection
 {
-    /**
-     * @var string
-     */
-    protected $errorDetails;
+    protected string $errorDetails;
 
     /**
      * @return PDO database connection
