f05ec7bf66c2217d977d9468e1b48afc71dfe766
Juan David Rueda Quiroga <jrueda@oneago.com>
2021-10-27 19:05:05 -0500

    Now can add new PDO Options

--- core/Database/Connection.php
+++ core/Database/Connection.php
@@ -11,15 +11,18 @@ abstract class Connection
     protected string $errorDetails;
 
     /**
-     * @return PDO database connection
+     * @param array $PDOOptions
+     * @return PDO
      */
-    protected static function init(): PDO
+    protected static function init(array $PDOOptions = []): PDO
     {
         $options = [
             PDO::ATTR_EMULATE_PREPARES => false, // turn off emulation mode for "real" prepared statements
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
         ];
+
+        $options = array_merge($options, $PDOOptions);
 
         try {
             return new PDO("mysql:host={$_ENV["DB_HOST"]};port={$_ENV["DB_PORT"]};dbname={$_ENV["DB_NAME"]};charset=utf8", $_ENV["DB_USER"], $_ENV["DB_PASS"], $options);
